442fb01ff061014ddc0bad85c459e1573cac313e
awais18311 <awaiskhawaja2004@gmail.com>
2025-07-13 09:41:33 -0700

    Fix routing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage';
 import Auth from './Components/Auth';
 import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   );
 }
 
