52e429aa00a674fde2571ed9a0bee2b1122942e5
awais18311 <awaiskhawaja2004@gmail.com>
2025-07-13 09:19:48 -0700

    Initial commit - Images to PDF Converter

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/images_to_pdf_converter/', 
+  base: '/imagestopdfconverter/', 
   plugins: [react()],
 })
